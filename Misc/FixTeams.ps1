de217d04240e2ee1897e3dc34997eae47ae2447b
EntrustRW <richard.woods@entrustit.co.uk>
2024-12-04 09:33:44 +0000

    Create FixTeams.ps1

---
+++ Misc/FixTeams.ps1
@@ -0,0 +1,21 @@
+Get-AppxProvisionedPackage -Online `
+	| where {$_.Displayname -like "*teams*"} `
+		| Remove-AppxProvisionedPackage -Online
+
+Get-AppxPackage *teams* -AllUsers `
+	| Remove-AppxPackage -AllUsers
+
+Get-ChildItem "C:\Windows\Temp\" -Recurse `
+    | Remove-Item -Recurse -Force
+
+Get-ChildItem "C:\ProgramData\Temp\" -Recurse `
+    | Remove-Item -Recurse -Force
+
+(gwmi win32_product `
+    | where {$_.name -match "Teams"}).Uninstall
+
+Remove-Item "C:\Program Files (x86)\Microsoft\Teams" -Recurse -Force
+
+Remove-Item "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force
+
+Start-Process Powershell -args "C:\source\software\Install-TeamsNewonServer2019-x86.ps1"
