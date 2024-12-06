a16f3bdee1e76e7762b14b6c6869b4fbfd1331ac
EntrustRW <richard.woods@entrustit.co.uk>
2024-12-06 15:00:16 +0000

    Update Debloat-Win11.ps1

--- Misc/Debloat-Win11.ps1
+++ Misc/Debloat-Win11.ps1
@@ -1,1 +1,43 @@
+$ProvisonedPackages = @(
+    'Microsoft.OutlookForWindows',
+    'Microsoft.MicrosoftSolitaireCollection',
+    'Microsoft.ZuneMusic',
+    'Microsoft.ZuneVideo',
+    'MicrosoftCorporationII.QuickAssist',
+    'Microsoft.BingNews',
+    'Microsoft.BingWeather',
+    'Microsoft.GamingApp',
+    'Microsoft.PowerAutomateDesktop',
+    'Microsoft.MicrosoftSolitaireCollection',
+    'Microsoft.MicrosoftOfficeHub',
+    'Microsoft.Getstarted',
+    'Clipchamp.Clipchamp',
+    'Microsoft.Windows.DevHome'
+)
+
+$AppxPackages = @(
+    'Microsoft.ZuneMusic',
+    'Microsoft.ZuneVideo',
+    'Microsoft.OutlookForWindows',
+    'Microsoft.BingNews',
+    'Microsoft.BingWeather',
+    'Microsoft.GamingApp',
+    'Microsoft.PowerAutomateDesktop',
+    'Microsoft.MicrosoftSolitaireCollection',
+    'Microsoft.MicrosoftOfficeHub',
+    'Microsoft.Getstarted',
+    'Clipchamp.Clipchamp',
+    'Microsoft.Windows.DevHome'
+)
+
+FOREACH ($app in $ProvisionedPackages) {
+    get-appxprovisionedpackage -online `
+        | Where-Object {$_.Displayname -match $app} `
+            | remove-appxprovisionedpackage -Online -ErrorAction SilentlyContinue
+}
+
+FOREACH ($package in $AppxPackages) {
+    Get-AppxPackage -AllUsers $package `
+        | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue
+}
 
