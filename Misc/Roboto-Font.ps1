d6d66a8fa9ae85d5b83b0dad06da24b2479db724
EntrustRW <richard.woods@entrustit.co.uk>
2025-03-03 12:40:45 +0000

    Create Roboto-Font.ps1

---
+++ Misc/Roboto-Font.ps1
@@ -0,0 +1,101 @@
+##################################################################################################################################################################################################
+# Installing fonts through Powershell requires specific COM properties to be added to the files before moving, as well as registering them in specific parts of the registry.                    #
+# Instead of spending ages working out how to get the COM property to work, I've used a script I found online: https://www.alkanesolutions.co.uk/2021/12/06/installing-fonts-with-powershell/    #
+##################################################################################################################################################################################################
+
+
+function Install-Font {  
+    param  
+    (  
+        [System.IO.FileInfo]$fontFile  
+    )  
+          
+        try { 
+    
+            #get font name
+            $gt = [Windows.Media.GlyphTypeface]::new($fontFile.FullName)
+            $family = $gt.Win32FamilyNames['en-us']
+            if ($null -eq $family) { $family = $gt.Win32FamilyNames.Values.Item(0) }
+            $face = $gt.Win32FaceNames['en-us']
+            if ($null -eq $face) { $face = $gt.Win32FaceNames.Values.Item(0) }
+            $fontName = ("$family $face").Trim() 
+               
+            switch ($fontFile.Extension) {  
+                ".ttf" {$fontName = "$fontName (TrueType)"}  
+                ".otf" {$fontName = "$fontName (OpenType)"}  
+            }  
+    
+            write-host "Installing font: $fontFile with font name '$fontName'"
+    
+            If (!(Test-Path ("$($env:windir)\Fonts\" + $fontFile.Name))) {  
+                write-host "Copying font: $fontFile"
+                Copy-Item -Path $fontFile.FullName -Destination ("$($env:windir)\Fonts\" + $fontFile.Name) -Force 
+            } else {  write-host "Font already exists: $fontFile" }
+    
+            If (!(Get-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue)) {  
+                write-host "Registering font: $fontFile"
+                New-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -PropertyType string -Value $fontFile.Name -Force -ErrorAction SilentlyContinue | Out-Null  
+            } else {  write-host "Font already registered: $fontFile" }
+               
+            [System.Runtime.Interopservices.Marshal]::ReleaseComObject($oShell) | out-null 
+            Remove-Variable oShell               
+                 
+        } catch {            
+            write-host "Error installing font: $fontFile. " $_.exception.message
+        }
+        
+     } 
+     
+    
+    function Uninstall-Font {  
+    param  
+    (  
+        [System.IO.FileInfo]$fontFile  
+    )  
+          
+        try { 
+    
+        #get font name
+            $gt = [Windows.Media.GlyphTypeface]::new($fontFile.FullName)
+            $family = $gt.Win32FamilyNames['en-us']
+            if ($null -eq $family) { $family = $gt.Win32FamilyNames.Values.Item(0) }
+            $face = $gt.Win32FaceNames['en-us']
+            if ($null -eq $face) { $face = $gt.Win32FaceNames.Values.Item(0) }
+            $fontName = ("$family $face").Trim()
+               
+            switch ($fontFile.Extension) {  
+                ".ttf" {$fontName = "$fontName (TrueType)"}  
+                ".otf" {$fontName = "$fontName (OpenType)"}  
+            }  
+    
+            write-host "Uninstalling font: $fontFile with font name '$fontName'"
+    
+            If (Test-Path ("$($env:windir)\Fonts\" + $fontFile.Name)) {  
+                write-host "Removing font: $fontFile"
+                Remove-Item -Path "$($env:windir)\Fonts\$($fontFile.Name)" -Force 
+            } else {  write-host "Font does not exist: $fontFile" }
+    
+            If (Get-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -ErrorAction SilentlyContinue) {  
+                write-host "Unregistering font: $fontFile"
+                Remove-ItemProperty -Name $fontName -Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\Fonts" -Force                      
+            } else {  write-host "Font not registered: $fontFile" }
+               
+            [System.Runtime.Interopservices.Marshal]::ReleaseComObject($oShell) | out-null 
+            Remove-Variable oShell               
+                 
+        } catch {            
+            write-host "Error uninstalling font: $fontFile. " $_.exception.message
+        }        
+    }  
+
+    ######################################################################################################################################################################
+    # IWR is short for Invoke-WebRequest, basically telling Powershell to request this file from our static website, and save it in Temp with the name "Roboto.zip"      #                            
+    # Expanding the archive in Temp, then doing a ForEach loop to go through each file that ends in .ttf or .otf, and install the font                                   #
+    ######################################################################################################################################################################
+    Expand-Archive -Path $Env:temp\Roboto.zip -Destination $Env:temp
+    $fontDirectory = "$Env:temp\Roboto"
+    #Loop through fonts in the same directory as the script and install/uninstall them
+    foreach ($FontItem in (Get-ChildItem -Path $fontDirectory | 
+    Where-Object {($_.Name -like '*.ttf') -or ($_.Name -like '*.otf') })) {  
+        Install-Font -fontFile $FontItem.FullName  
+    }  
