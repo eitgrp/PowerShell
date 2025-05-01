1f623e0b25c1b7d125372fce79f785414c87cf5b
EntrustRW <richard.woods@entrustit.co.uk>
2025-05-01 12:05:31 +0100

    Update Update-PDFSam.ps1

--- Update Scripts/Update-PDFSam.ps1
+++ Update Scripts/Update-PDFSam.ps1
@@ -1,8 +1,10 @@
 $LatestVersion = [system.version]::Parse((Get-EvergreenApp SoberLemurPDFSamBasic | where {($_.Architecture -eq "x64") -and ($_.Type -eq "msi")}).Version)
 $DownloadURL = (Get-EvergreenApp SoberLemurPDFSamBasic | where {($_.Architecture -eq "x64") -and ($_.Type -eq "msi")}).uri
-
-$InstalledVersion = [system.version]::Parse((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like "PDFSam*"}).DisplayVersion)
-
+try {
+    $InstalledVersion = [system.version]::Parse((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like "PDFSam*"}).DisplayVersion)
+} Catch {
+    $InstalledVersion = [system.version]::Parse((Get-ItemProperty "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\" | where {$_.DisplayName -like "PDFSam*"}).DisplayVersion)
+}
 if ($InstalledVersion -lt $LatestVersion) {
     $Test = Test-Path -path "C:\source\Software\PDFSam\"
     if (!$Test) {
