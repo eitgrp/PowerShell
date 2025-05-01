1f30e27525e50e2ec8f02f3790c8cb7020ca0fea
EntrustRW <richard.woods@entrustit.co.uk>
2025-05-01 11:53:53 +0100

    Create Update-PDFSam.ps1

---
+++ Update Scripts/Update-PDFSam.ps1
@@ -0,0 +1,13 @@
+$LatestVersion = [system.version]::Parse((Get-EvergreenApp SoberLemurPDFSamBasic | where {($_.Architecture -eq "x64") -and ($_.Type -eq "msi")}).Version)
+$DownloadURL = (Get-EvergreenApp SoberLemurPDFSamBasic | where {($_.Architecture -eq "x64") -and ($_.Type -eq "msi")}).uri
+
+$InstalledVersion = [system.version]::Parse((Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | where {$_.DisplayName -like "PDFSam*"}).DisplayVersion)
+
+if ($InstalledVersion -lt $LatestVersion) {
+    $Test = Test-Path -path "C:\source\Software\PDFSam\"
+    if (!$Test) {
+        mkdir C:\source\Software\PDFSam
+    }
+    iwr $DownloadURL -OutFile "C:\source\Software\PDFSam\PDFSam-$LatestVersion.msi"
+    Start-Process msiexec -args "/i `"C:\source\Software\PDFSam\PDFSam-$LatestVersion.msi`" /qn" -Wait
+}
