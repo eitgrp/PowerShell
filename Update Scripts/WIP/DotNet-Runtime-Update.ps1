730f01960e849d49329414e0953444b6da72285b
EntrustRW <richard.woods@entrustit.co.uk>
2025-05-07 13:58:52 +0100

    Update DotNet-Runtime-Update.ps1

--- Update Scripts/WIP/DotNet-Runtime-Update.ps1
+++ Update Scripts/WIP/DotNet-Runtime-Update.ps1
@@ -1,54 +1,38 @@
-$Pathx64 = "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\"
-$installedx64 = Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\*"
+$installed = (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\*") + (Get-ItemProperty "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x86\sharedfx\*").Property 
 $PackagesToCheck = @()
-$VersionsToCheck = @()
 
- if ($installedx64.PSChildName -Contains "Microsoft.WindowsDesktop.App") {
-    Write-host "found Microsoft.WindowsDesktop.App"
-        $PackageName = "Microsoft.WindowsDesktop.App"
-        $PackagesToCheck += $PackageName
-        $Versions = (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName").Property
-        if ($Versions.count -gt 1) {
-            foreach ($Version in $Versions) {
-                $PackagesToCheck += $PackageName
-                $VersionsToCheck += $Version
-            }
-        } ELSE {
-            $VersionsToCheck += (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName").Property
-        }
-    } ELSE {
-        if ($installedx64.PSChildName -contains "Microsoft.NETCore.App") {
-            $PackageName = "Microsoft.NETCore.App"
-            $PackagesToCheck += $PackageName
-            $Versions = (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName\").Property
-            if ($Versions.count -gt 1) {
-                foreach ($Version in $Versions) {
-                    $PackagesToCheck += $PackageName
-                    $VersionsToCheck += $Version
-                }
-            } ELSE {
-                $VersionsToCheck += (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName").Property
-            }
+ if ($installed.PSChildName -Contains "Microsoft.WindowsDesktop.App") {
+    $PackageName = "Microsoft.WindowsDesktop.App"
+    $Versions = (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName" -erroraction silentlycontinue).Property + (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x86\sharedfx\$PackageName" -erroraction silentlycontinue).Property 
+    foreach ($Version in $Versions) {
+        $Package = New-Object -TypeName pscustomobject
+        $Package | Add-Member -MemberType NoteProperty -Name "PackageName" -value $PackageName
+        $Package | Add-Member -MemberType NoteProperty -Name "Version" -value $Version
+        $PackagesToCheck += $Package
     }
-    if ($installedx64.PSChildName -Contains "Microsoft.AspNetCore.App") {
-        $PackageName = "Microsoft.AspNetCore.App"
-        $PackagesToCheck += $PackageName
-        $Versions = (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName").Property
-        if ($Versions.count -gt 1) {
-            foreach ($Version in $Versions) {
-                $PackagesToCheck += $PackageName
-                $VersionsToCheck += $Version
-            }
-        } ELSE {
-            $VersionsToCheck += (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName").Property
-        }
+} ELSEif ($installed.PSChildName -contains "Microsoft.NETCore.App") {
+    $PackageName = "Microsoft.NETCore.App"
+    $Versions = (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName" -erroraction silentlycontinue).Property + (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x86\sharedfx\$PackageName" -erroraction silentlycontinue).Property
+    foreach ($Version in $Versions) {
+        $Package = New-Object -TypeName pscustomobject
+        $Package | Add-Member -MemberType NoteProperty -Name "PackageName" -value $PackageName
+        $Package | Add-Member -MemberType NoteProperty -Name "Version" -value $Version
+        $PackagesToCheck += $Package
+    }
+} if ($installed.PSChildName -Contains "Microsoft.AspNetCore.App") {
+    $PackageName = "Microsoft.AspNetCore.App"
+    $Versions = (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x64\sharedfx\$PackageName" -ErrorAction SilentlyContinue).Property + (get-item "HKLM:\SOFTWARE\WOW6432Node\dotnet\Setup\InstalledVersions\x86\sharedfx\$PackageName" -ErrorAction SilentlyContinue).Property
+    foreach ($Version in $Versions) {
+        $Package = New-Object -TypeName pscustomobject
+        $Package | Add-Member -MemberType NoteProperty -Name "PackageName" -value $PackageName
+        $Package | Add-Member -MemberType NoteProperty -Name "Version" -value $Version
+        $PackagesToCheck += $Package
     }
 }
-Write-Host $PackagesToCheck
-Write-host $VersionsTocheck
-$i = 0
-foreach ($Package in $PackagesToCheck) {
-    Switch ($VersionsToCheck[$i].Split(".")[0]) {
+
+foreach ($Package in $PackagesToCheck | where Version -like "*.*") {
+    $pattern = 'runtime-Desktop-\d.\d.\d+'
+    Switch ($Package.Version.Split(".")[0]) {
         (8) {
             $url = "https://dotnet.microsoft.com/en-us/download/dotnet/8.0"
         }
@@ -57,45 +41,43 @@ foreach ($Package in $PackagesToCheck) {
         }
         (10) {
             $url = "https://dotnet.microsoft.com/en-us/download/dotnet/10.0"
+            $Pattern = 'runtime-Desktop-\d\d.\d.\d+'
         }
     }
-    Switch ($Package) {
-        ("Microsoft.WindowsDesktop.App") {
-            
-            $dotnetVersionWebAddress = (Invoke-WebRequest $url -usebasicparsing).links.href
-            $runtimeDesktop8 = ($dotnetVersionWebAddress | Select-String -Pattern 'runtime-Desktop-\d.\d.\d+-windows-x64-installer').Matches.Value[0]
-            $VerString = ($runtimeDesktop8 | select-string -pattern '\d.\d.\d+').matches.value
-            $LatestVer = [system.version]::Parse($VerString)
-            $installedVer = [system.version]::Parse($VersionstoCheck[$i])
-            if ($installedVer -lt $latestVer) {
-                Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/$VerString/windowsdesktop-runtime-$VerString-win-x64.exe" -OutFile C:\source\dotnet-WindowsDesktop-$VerString.exe
-                Start-Process C:\source\dotnet-WindowsDesktop-$VerString.exe -args "/install /Quiet"
+    $dotnetVersionWebAddress = (Invoke-WebRequest $url -usebasicparsing).links.href
+    $VerString = ($dotnetVersionWebAddress | Select-String -Pattern $Pattern).Matches.Value[0].Trim("runtime-desktop-")
+    $LatestVer = [system.version]::Parse($VerString)
+    $installedVer = [system.version]::Parse($Package.Version)
+    if ($installedVer -lt $latestVer) {
+        New-Item "C:\source\Software" -ItemType Directory -ErrorAction SilentlyContinue
+        Switch ($Package.PackageName) {
+            ("Microsoft.WindowsDesktop.App") {
+                $urls = @(
+                    "https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/$VerString/windowsdesktop-runtime-$VerString-win-x64.exe",
+                    "https://builds.dotnet.microsoft.com/dotnet/WindowsDesktop/$VerString/windowsdesktop-runtime-$VerString-win-x86.exe"
+                )
+            }
+            ("Microsoft.AspNetCore.App") {
+                $urls = @(
+                    "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$VerString/aspnetcore-runtime-$VerString-win-x64.exe",
+                    "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$VerString/aspnetcore-runtime-$VerString-win-x86.exe"
+                )
+                }
+            ("Microsoft.NETCore.App") {
+                $urls = @(
+                    "https://builds.dotnet.microsoft.com/dotnet/Runtime/$VerString/dotnet-runtime-$VerString-win-x64.exe",
+                    "https://builds.dotnet.microsoft.com/dotnet/Runtime/$VerString/dotnet-runtime-$VerString-win-x86.exe"
+                )
             }
         }
-
-        ("Microsoft.AspNetCore.App") {
-            
-            $dotnetVersionWebAddress = (Invoke-WebRequest $url -usebasicparsing).links.href
-            $runtimeDesktop8 = ($dotnetVersionWebAddress | Select-String -Pattern 'runtime-aspnetcore-\d.\d.\d+').Matches.Value[0]
-            $VerString = ($runtimeDesktop8 | select-string -pattern '\d.\d.\d+').matches.value
-            $LatestVer = [system.version]::Parse($VerString)
-            $installedVer = [system.version]::Parse($VersionstoCheck[$i])
-            if ($installedVer -lt $latestVer) {
-                Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/aspnetcore/Runtime/$VerString/aspnetcore-runtime-$VerString-win-x64.exe" -OutFile C:\source\dotnet-ASP-$VerString.exe
-                Start-Process C:\source\Dotnet-ASP-$VerString.exe -args "/install /Quiet"
-            }
-        }
-        ("Microsoft.NETCore.App") {
-            
-            $dotnetVersionWebAddress = (Invoke-WebRequest $url -usebasicparsing).links.href
-            $runtimeDesktop8 = ($dotnetVersionWebAddress | Select-String -Pattern 'runtime-\d.\d.\d+').Matches.Value[0]
-            $VerString = ($runtimeDesktop8 | select-string -pattern '\d.\d.\d+').matches.value
-            $LatestVer = [system.version]::Parse($VerString)
-            $installedVer = [system.version]::Parse($VersionstoCheck[$i])
-            if ($installedVer -lt $latestVer) {
-                Invoke-WebRequest "https://builds.dotnet.microsoft.com/dotnet/Runtime/$VerString/dotnet-runtime-$VerString-win-x64.exe" -OutFile C:\source\dotnet-Runtime-$VerString.exe
-                Start-Process C:\source\Dotnet-Runtime-$VerString.exe -args "/install /Quiet"
-            }
+        foreach ($DLUrl in $urls) {
+            $Split = $DLUrl.Split("/").count
+            $filepath = "C:\source\Software\" + ($DLUrl.Split("/")[$split - 1])
+            Invoke-WebRequest $DLUrl -OutFile $filepath
+            Start-Process $filepath -args "/install /quiet" -Wait
         }
     }
 }
+
+
+
