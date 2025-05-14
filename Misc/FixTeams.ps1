00fc0a3a454cd75a0e9659966fa4ac51b855ef65
EntrustRW <richard.woods@entrustit.co.uk>
2025-05-14 09:13:59 +0100

    Update FixTeams.ps1

--- Misc/FixTeams.ps1
+++ Misc/FixTeams.ps1
@@ -6,16 +6,16 @@ Get-AppxPackage *teams* -AllUsers `
 	| Remove-AppxPackage -AllUsers
 
 Get-ChildItem "C:\Windows\Temp\" -Recurse `
-    | Remove-Item -Recurse -Force
+    | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 
 Get-ChildItem "C:\ProgramData\Temp\" -Recurse `
-    | Remove-Item -Recurse -Force
+    | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 
 (gwmi win32_product `
     | where {$_.name -match "Teams"}).Uninstall
 
-Remove-Item "C:\Program Files (x86)\Microsoft\Teams" -Recurse -Force
+Remove-Item "C:\Program Files (x86)\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
 
-Remove-Item "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force
+Remove-Item "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
 
 Start-Process Powershell -args "C:\source\software\Install-TeamsNewonServer2019-x86.ps1" -wait
