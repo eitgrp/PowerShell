694ff45e78dbbd912f620120c267475f345e9f94
EntrustRW <richard.woods@entrustit.co.uk>
2025-05-14 08:59:17 +0100

    Update Debloat-Win11.ps1

--- Misc/Debloat-Win11.ps1
+++ Misc/Debloat-Win11.ps1
@@ -36,7 +36,14 @@ $AppxPackages = @(
     'Microsoft.Getstarted',
     'Clipchamp.Clipchamp',
     'Microsoft.Windows.DevHome',
-    'MicrosoftTeams' # This is Teams (Personal)
+    'MicrosoftTeams', # This is Teams (Personal)
+    'Microsoft.Xbox.TCUI',
+    'Microsoft.XboxGameOverlay',
+    'Microsoft.XboxIdentityProvider',
+    'Microsoft.XboxSpeechToTextOverlay',
+    'Microsoft.XboxGamingOverlay',
+    'MicrosoftCorporationII.QuickAssist',
+    'Microsoft.BingSearch'
 )
 
 FOREACH ($app in $ProvisionedPackages) {
