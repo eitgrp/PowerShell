5a261365196b56ae5c5bc356a6f2e8f06dbf91e3
EntrustRW <richard.woods@entrustit.co.uk>
2025-05-14 09:13:08 +0100

    Update FixTeams.ps1

--- Misc/FixTeams.ps1
+++ Misc/FixTeams.ps1
@@ -18,4 +18,4 @@ Remove-Item "C:\Program Files (x86)\Microsoft\Teams" -Recurse -Force
 
 Remove-Item "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force
 
-Start-Process Powershell -args "C:\source\software\Install-TeamsNewonServer2019-x86.ps1"
+Start-Process Powershell -args "C:\source\software\Install-TeamsNewonServer2019-x86.ps1" -wait
