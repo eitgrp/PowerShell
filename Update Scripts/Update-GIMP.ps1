fe64cee715a2d5edf3b8f5fdf93747539873dcc1
EntrustRW <richard.woods@entrustit.co.uk>
2025-07-30 11:17:55 +0100

    Added /ALLUSERS switch to install

--- Update Scripts/Update-GIMP.ps1
+++ Update Scripts/Update-GIMP.ps1
@@ -103,7 +103,7 @@ If (Test-Path $Installer -PathType Leaf) {
      #$logfile = "$InstallerPath\$Product-$($evergreenapp.Architecture).log"
      #Start-Process "msiexec.exe" -ArgumentList  "/i $Installer /qn /l*v $logfile" -NoNewWindow -Wait
      Start-Process "$($UninstallString[0].Replace('"',''))" -args "/$($UninstallString[1])" -Wait
-     Start-Process $Installer -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-" -Wait
+     Start-Process $Installer -ArgumentList "/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /ALLUSERS /SP-" -Wait
      If ($rds) {
         Start-Process 'change.exe' -ArgumentList "user /execute" -NoNewWindow -Wait
      }
