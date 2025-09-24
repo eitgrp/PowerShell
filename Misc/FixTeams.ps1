81a96fa2d2a30766f092c663e63e98e90ce72aa3
nd-KH <nd@keepstar.xyz>
2025-09-24 13:37:36 +0100

    added the 64 version of Fix Teams.

--- Misc/FixTeams.ps1
+++ Misc/FixTeams.ps1
@@ -6,16 +6,16 @@ Get-AppxPackage *teams* -AllUsers `
 	| Remove-AppxPackage -AllUsers
 
 Get-ChildItem "C:\Windows\Temp\" -Recurse `
-    | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+    | Remove-Item -Recurse -Force
 
 Get-ChildItem "C:\ProgramData\Temp\" -Recurse `
-    | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+    | Remove-Item -Recurse -Force
 
 (gwmi win32_product `
     | where {$_.name -match "Teams"}).Uninstall
 
-Remove-Item "C:\Program Files (x86)\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "C:\Program Files\Microsoft\Teams" -Recurse -Force
 
-Remove-Item "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item "HKLM:\SOFTWARE\Microsoft\Teams" -Recurse -Force
 
-iwr "https://raw.githubusercontent.com/eitgrp/PowerShell/refs/heads/master/Update%20Scripts/Install-TeamsNewonServer2019-x86.ps1" | iex
+Start-Process Powershell -args "C:\source\software\Install-TeamsNewonServer2019-x64.ps1"
