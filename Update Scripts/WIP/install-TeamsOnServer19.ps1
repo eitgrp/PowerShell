fe0c5d7b40a805415f33c46a6fb3bed71214c211
eit-ND <nick.dodd@entrustit.co.uk>
2025-10-14 17:58:24 +0100

    Create install-TeamsOnServer19.ps1

---
+++ Update Scripts/WIP/install-TeamsOnServer19.ps1
@@ -0,0 +1,13 @@
+<# 
+
+This is a rewrite of the current script to make it more reliable and maintainable,  I aim to get this working for multiple versions of WinServ.  as well.
+
+aims of the script:
+
+    Uninstall broken installation
+    clear any temp and left over files
+    download the machine-wide installer
+    install the new version
+    verify the installation.
+
+#>
