caf98caaba7ff2a8e31f0c3ebd5b249603dfb391
eit-ND <nick.dodd@entrustit.co.uk>
2026-01-05 13:33:36 +0000

    created the logging and started the uninstall function

--- Update Scripts/WIP/install-TeamsOnServer19.ps1
+++ Update Scripts/WIP/install-TeamsOnServer19.ps1
@@ -11,3 +11,44 @@ aims of the script:
     verify the installation.
 
 #>
+
+$testLogfile = test-path "C:\Source\Log-file.txt"
+$testSource = test-path "C:\Source"
+$log = "C:\Source\Log-file.txt"
+
+if($testLogfile -eq $false){
+    if($testSource -eq $false){
+        New-Item -ItemType Directory -Path "C:\" -Name "Source"
+    }
+    new-item -Path "C:\Source" -ItemType File -Name "Log-file.txt"
+}
+
+Start-Transcript -NoClobber -Append -Path "C:\Source\Log-file.txt"
+
+Uninstall-BrokenTeams
+
+$CompArch = (get-ciminstance -ClassName Win32_OperatingSystem).OSArchitecture
+
+if($CompArch -like "*32*"){
+    Install-TeamsOn2019_32Bit
+}else{
+    Install-TeamsOn2019_64Bit
+}
+
+function Install-TeamsOn2019_32Bit {
+    
+}
+
+function Install-TeamsOn2019_64Bit {
+    
+}
+
+function Uninstall-BrokenTeams {
+    "$(get-date) - uninstalling teams" | Add-Content $log
+    try {
+        Get-AppxPackage | where-object {$_.name -like "*teams*"} | Remove-AppxPackage -ErrorAction Stop
+    }
+    catch {
+        "unable to uninstall teams with remove-appxpackage" | Add-Content $log
+    }
+}
